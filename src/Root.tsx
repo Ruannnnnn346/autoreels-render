974d232cbafcb549fc50b394094f6b2f48ccccd8
Ruannnnnn346 <nruan939@gmail.com>
2026-01-07 07:59:07 -0300

    Update Root.tsx

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,21 +1,18 @@
-import React from 'react';
 import { Composition } from 'remotion';
-import { Video } from './Video';
+import { Video, VideoProps } from './Video';
 
-export const RemotionRoot: React.FC = () => {
+export const RemotionRoot = () => {
   return (
-    <>
-      <Composition
-        id="AutoReelsVideo"
-        component={Video}
-        durationInFrames={300}
-        fps={30}
-        width={1080}
-        height={1920}
-        defaultProps={{
-          scenes: [],
-        }}
-      />
-    </>
+    <Composition
+      id="AutoReelsVideo"
+      component={Video}
+      durationInFrames={300}
+      fps={30}
+      width={1080}
+      height={1920}
+      defaultProps={{
+        scenes: [],
+      } as VideoProps}
+    />
   );
 };
