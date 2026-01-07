f1666ce1286aa46f7af9902ac5f7ab763d2b14a1
Ruannnnnn346 <nruan939@gmail.com>
2026-01-07 08:48:39 -0300

    Update Root.tsx

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import { Composition } from 'remotion';
-import { Video } from './Video';
+import { AutoReelsVideo } from './Video';
+
+// Definindo o tipo das props
+interface SceneData {
+  type: string;
+  content: string;
+  image_url?: string | null;
+}
+
+interface VideoProps {
+  scenes: SceneData[];
+}
 
 export const RemotionRoot: React.FC = () => {
   return (
     <Composition
       id="MainVideo"
-      component={Video}
+      component={AutoReelsVideo}
       durationInFrames={900}
       fps={30}
       width={1080}
       height={1920}
       defaultProps={{
-        scenes: [],
+        scenes: [] as SceneData[],
       }}
     />
   );
