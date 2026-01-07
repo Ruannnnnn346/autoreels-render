26f69649cc7ce8062751c421efd39d458e982671
Ruannnnnn346 <nruan939@gmail.com>
2026-01-07 09:31:42 -0300

    Update remotion.config.ts

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,4 +1,6 @@
 import { Config } from '@remotion/cli/config';
 
-Config.setVideoImageFormat('jpeg');
-Config.setOverwriteOutput(true);
+Config.setConcurrency(1);
+Config.setChromiumHeadlessMode('shell');
+Config.setVideoCodec('h264');
+Config.setCodecOption('crf', '28');
