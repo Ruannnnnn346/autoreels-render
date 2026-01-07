34aa80539f30ac962f3eea97b2755486bdf3bf2a
Ruannnnnn346 <nruan939@gmail.com>
2026-01-07 08:14:17 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
 import { registerRoot } from 'remotion';
 import { RemotionRoot } from './Root';
 
-if (!RemotionRoot) {
-  throw new Error('RemotionRoot is undefined!');
-}
-
 registerRoot(RemotionRoot);
